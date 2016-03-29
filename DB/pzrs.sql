c93c62835d0cbcc172ea826975a542d1a09bfbfd
Patrik Žižka <patrik.zizka.s@gmail.com>
2016-03-29 17:50:25 +0200

    new matches

--- DB/pzrs.sql
+++ DB/pzrs.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.4.14
+-- version 4.5.1
 -- http://www.phpmyadmin.net
 --
 -- Počítač: 127.0.0.1
--- Vytvořeno: Pon 28. bře 2016, 23:47
--- Verze serveru: 5.6.26
--- Verze PHP: 5.6.12
+-- Vytvořeno: Úte 29. bře 2016, 17:49
+-- Verze serveru: 10.1.9-MariaDB
+-- Verze PHP: 5.6.15
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -26,20 +26,20 @@ SET time_zone = "+00:00";
 -- Struktura tabulky `pzrs_enemy`
 --
 
-CREATE TABLE IF NOT EXISTS `pzrs_enemy` (
+CREATE TABLE `pzrs_enemy` (
   `id` int(11) NOT NULL,
   `teamA` text COLLATE utf8_unicode_ci NOT NULL,
   `teamALogo` text COLLATE utf8_unicode_ci NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `active` int(11) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=38 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Vypisuji data pro tabulku `pzrs_enemy`
 --
 
 INSERT INTO `pzrs_enemy` (`id`, `teamA`, `teamALogo`, `created_at`, `active`) VALUES
-(8, 'eTuba', 'upload/enemy\\hydrangeas.jpg', '0000-00-00 00:00:00', 0),
+(8, 'eTuba', 'upload/enemy\\1400154456053082.jpg', '0000-00-00 00:00:00', 0),
 (9, 'Penguinsasdasd', 'upload/enemy\\lighthouse.jpg', '0000-00-00 00:00:00', 0);
 
 -- --------------------------------------------------------
@@ -48,7 +48,7 @@ INSERT INTO `pzrs_enemy` (`id`, `teamA`, `teamALogo`, `created_at`, `active`) VA
 -- Struktura tabulky `pzrs_events`
 --
 
-CREATE TABLE IF NOT EXISTS `pzrs_events` (
+CREATE TABLE `pzrs_events` (
   `id` int(11) NOT NULL,
   `title` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
   `teaser` text COLLATE utf8_unicode_ci NOT NULL,
@@ -66,7 +66,7 @@ CREATE TABLE IF NOT EXISTS `pzrs_events` (
 -- Struktura tabulky `pzrs_gallery`
 --
 
-CREATE TABLE IF NOT EXISTS `pzrs_gallery` (
+CREATE TABLE `pzrs_gallery` (
   `id` int(11) NOT NULL,
   `name` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
   `private` int(11) NOT NULL,
@@ -79,7 +79,7 @@ CREATE TABLE IF NOT EXISTS `pzrs_gallery` (
 -- Struktura tabulky `pzrs_images`
 --
 
-CREATE TABLE IF NOT EXISTS `pzrs_images` (
+CREATE TABLE `pzrs_images` (
   `id` int(11) NOT NULL,
   `name` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
   `galleryid` int(100) NOT NULL,
@@ -91,31 +91,29 @@ CREATE TABLE IF NOT EXISTS `pzrs_images` (
 -- --------------------------------------------------------
 
 --
--- Struktura tabulky `pzrs_mapy`
+-- Struktura tabulky `pzrs_maps`
 --
 
-CREATE TABLE IF NOT EXISTS `pzrs_mapy` (
+CREATE TABLE `pzrs_maps` (
   `id` int(11) NOT NULL,
-  `name` text COLLATE utf8_unicode_ci NOT NULL,
+  `mapName` text COLLATE utf8_unicode_ci NOT NULL,
   `info` text COLLATE utf8_unicode_ci NOT NULL,
-  `img` text COLLATE utf8_unicode_ci NOT NULL,
+  `mapImg` text COLLATE utf8_unicode_ci NOT NULL,
   `active` int(11) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
--- Vypisuji data pro tabulku `pzrs_mapy`
+-- Vypisuji data pro tabulku `pzrs_maps`
 --
 
-INSERT INTO `pzrs_mapy` (`id`, `name`, `info`, `img`, `active`) VALUES
-(1, 'Dust2', 'Pisky pisky pisky', 'upload/maps/dust2.jpg', 1),
-(2, 'Inferno', 'Pisky pisky pisky', 'upload/maps/inferno.jpg', 1),
-(3, 'Train', 'Pisky pisky pisky', 'upload/maps/train.jpg', 1),
-(4, 'Nuke', 'Pisky pisky pisky', 'upload/maps/nuke.jpg', 1),
-(5, 'Overpass', 'Pisky pisky pisky', 'upload/maps/over.jpg', 1),
-(6, 'Cache', 'Pisky pisky pisky', 'upload/maps/cache.jpg', 1),
-(7, 'Mirage', 'Pisky pisky pisky', 'upload/maps/mirage.jpg', 1),
-(8, 'Cobblestone', 'Pisky pisky pisky', 'upload/maps/coble.jpg', 1),
-(9, 'Season', 'Pisky pisky pisky', 'upload/maps/season.jpg', 1);
+INSERT INTO `pzrs_maps` (`id`, `mapName`, `info`, `mapImg`, `active`) VALUES
+(23, 'Dust2', '', 'upload/maps/dust2.jpg', 0),
+(24, 'Cache', '', 'upload/maps/cache.jpg', 0),
+(25, 'Train', '', 'upload/maps/train.jpg', 0),
+(26, 'Inferno', '', 'upload/maps/inferno.jpg', 0),
+(27, 'Mirage', '', 'upload/maps/mirage.jpg', 0),
+(28, 'Cobblestone', '', 'upload/maps/cobblestone.jpg', 0),
+(29, 'Overpass', '', 'upload/maps/overpass.png', 0);
 
 -- --------------------------------------------------------
 
@@ -123,17 +121,18 @@ INSERT INTO `pzrs_mapy` (`id`, `name`, `info`, `img`, `active`) VALUES
 -- Struktura tabulky `pzrs_matches`
 --
 
-CREATE TABLE IF NOT EXISTS `pzrs_matches` (
+CREATE TABLE `pzrs_matches` (
   `id` int(11) NOT NULL,
   `teamA` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
   `teamB` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
   `resultA` int(11) NOT NULL,
   `resultB` int(11) NOT NULL,
+  `win` varchar(10) COLLATE utf8_unicode_ci NOT NULL DEFAULT 'L',
   `teamALogo` text COLLATE utf8_unicode_ci NOT NULL,
   `teamBLogo` text COLLATE utf8_unicode_ci NOT NULL,
-  `map1` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
-  `map2` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
-  `map3` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
+  `map1` text COLLATE utf8_unicode_ci NOT NULL,
+  `map2` text COLLATE utf8_unicode_ci NOT NULL,
+  `map3` text COLLATE utf8_unicode_ci NOT NULL,
   `resultAMap1` int(11) NOT NULL,
   `resultAMap2` int(11) NOT NULL,
   `resultAMap3` int(11) NOT NULL,
@@ -144,17 +143,14 @@ CREATE TABLE IF NOT EXISTS `pzrs_matches` (
   `created_at` timestamp NULL DEFAULT NULL,
   `edited_at` timestamp NULL DEFAULT NULL,
   `canBeEdited` int(11) NOT NULL DEFAULT '1'
-) ENGINE=InnoDB AUTO_INCREMENT=60 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Vypisuji data pro tabulku `pzrs_matches`
 --
 
-INSERT INTO `pzrs_matches` (`id`, `teamA`, `teamB`, `resultA`, `resultB`, `teamALogo`, `teamBLogo`, `map1`, `map2`, `map3`, `resultAMap1`, `resultAMap2`, `resultAMap3`, `resultBMap1`, `resultBMap2`, `resultBMap3`, `active`, `created_at`, `edited_at`, `canBeEdited`) VALUES
-(51, 'ghjghjghj', 'eTuba', 0, 0, 'upload/myTeams/penguins.jpg', 'upload/enemy\\hydrangeas.jpg', '', '', '', 0, 0, 0, 0, 0, 0, 0, '2016-03-27 16:00:51', '2016-03-27 16:51:40', 1),
-(54, 'Koaly', 'eTuba', 0, 0, 'upload/myTeams/koala.jpg', 'upload/enemy\\hydrangeas.jpg', '', '', '', 0, 0, 0, 0, 0, 0, 0, '2016-03-27 16:00:59', '2016-03-27 16:51:34', 1),
-(56, 'ghjghjghj', 'Penguinsasdasd', 0, 0, 'upload/myTeams/penguins.jpg', 'upload/enemy\\lighthouse.jpg', '', '', '', 0, 0, 0, 0, 0, 0, 0, '2016-03-27 16:47:03', NULL, 1),
-(59, 'Koaly', 'asd', 0, 0, 'upload/myTeams/koala.jpg', 'upload/enemy/chrysanthemum.jpg', '', '', '', 0, 0, 0, 0, 0, 0, 0, '2016-03-28 16:07:29', NULL, 0);
+INSERT INTO `pzrs_matches` (`id`, `teamA`, `teamB`, `resultA`, `resultB`, `win`, `teamALogo`, `teamBLogo`, `map1`, `map2`, `map3`, `resultAMap1`, `resultAMap2`, `resultAMap3`, `resultBMap1`, `resultBMap2`, `resultBMap3`, `active`, `created_at`, `edited_at`, `canBeEdited`) VALUES
+(92, 'IronBranch.CSGO', 'eTuba', 2, 1, 'W', 'upload/myTeams\\logo.png', 'upload/enemy\\1400154456053082.jpg', 'Mirage', 'Cache', 'Dust2', 16, 10, 16, 14, 16, 5, 0, '2016-03-29 15:46:20', NULL, 1);
 
 -- --------------------------------------------------------
 
@@ -162,7 +158,7 @@ INSERT INTO `pzrs_matches` (`id`, `teamA`, `teamB`, `resultA`, `resultB`, `teamA
 -- Struktura tabulky `pzrs_posts`
 --
 
-CREATE TABLE IF NOT EXISTS `pzrs_posts` (
+CREATE TABLE `pzrs_posts` (
   `id` int(11) NOT NULL,
   `title` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
   `teaser` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
@@ -175,7 +171,7 @@ CREATE TABLE IF NOT EXISTS `pzrs_posts` (
   `slug` text COLLATE utf8_unicode_ci NOT NULL,
   `active` int(11) NOT NULL DEFAULT '1',
   `res1` int(11) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Vypisuji data pro tabulku `pzrs_posts`
@@ -193,7 +189,7 @@ INSERT INTO `pzrs_posts` (`id`, `title`, `teaser`, `body`, `img`, `created_at`, 
 -- Struktura tabulky `pzrs_settings`
 --
 
-CREATE TABLE IF NOT EXISTS `pzrs_settings` (
+CREATE TABLE `pzrs_settings` (
   `id` int(11) NOT NULL,
   `fb_app_secret` text COLLATE utf8_unicode_ci NOT NULL,
   `fb_access_token` longtext COLLATE utf8_unicode_ci NOT NULL,
@@ -209,21 +205,21 @@ CREATE TABLE IF NOT EXISTS `pzrs_settings` (
 -- Struktura tabulky `pzrs_teams`
 --
 
-CREATE TABLE IF NOT EXISTS `pzrs_teams` (
+CREATE TABLE `pzrs_teams` (
   `id` int(11) NOT NULL,
   `teamA` text COLLATE utf8_unicode_ci NOT NULL,
   `teamALogo` text COLLATE utf8_unicode_ci NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `active` int(11) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=33 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Vypisuji data pro tabulku `pzrs_teams`
 --
 
 INSERT INTO `pzrs_teams` (`id`, `teamA`, `teamALogo`, `created_at`, `active`) VALUES
-(8, 'IronBranch.CSGO', 'upload/myTeams\\desert.jpg', '0000-00-00 00:00:00', 1),
-(29, 'Koaly', 'upload/myTeams/koala.jpg', '2016-03-27 15:54:36', 0),
+(8, 'IronBranch.CSGO', 'upload/myTeams\\logo.png', '0000-00-00 00:00:00', 1),
+(29, 'Koaly', 'upload/myTeams\\logo-zahrada2.jpg', '0000-00-00 00:00:00', 0),
 (31, 'ghjghjghj', 'upload/myTeams/penguins.jpg', '2016-03-27 15:59:40', 0);
 
 -- --------------------------------------------------------
@@ -232,7 +228,7 @@ INSERT INTO `pzrs_teams` (`id`, `teamA`, `teamALogo`, `created_at`, `active`) VA
 -- Struktura tabulky `pzrs_users`
 --
 
-CREATE TABLE IF NOT EXISTS `pzrs_users` (
+CREATE TABLE `pzrs_users` (
   `id` int(11) NOT NULL,
   `username` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
   `password` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
@@ -240,7 +236,7 @@ CREATE TABLE IF NOT EXISTS `pzrs_users` (
   `ip` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
   `role` int(11) NOT NULL,
   `active` int(11) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Vypisuji data pro tabulku `pzrs_users`
@@ -278,9 +274,9 @@ ALTER TABLE `pzrs_images`
   ADD PRIMARY KEY (`id`);
 
 --
--- Klíče pro tabulku `pzrs_mapy`
+-- Klíče pro tabulku `pzrs_maps`
 --
-ALTER TABLE `pzrs_mapy`
+ALTER TABLE `pzrs_maps`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -322,7 +318,7 @@ ALTER TABLE `pzrs_users`
 -- AUTO_INCREMENT pro tabulku `pzrs_enemy`
 --
 ALTER TABLE `pzrs_enemy`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=38;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 --
 -- AUTO_INCREMENT pro tabulku `pzrs_events`
 --
@@ -339,20 +335,20 @@ ALTER TABLE `pzrs_gallery`
 ALTER TABLE `pzrs_images`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
--- AUTO_INCREMENT pro tabulku `pzrs_mapy`
+-- AUTO_INCREMENT pro tabulku `pzrs_maps`
 --
-ALTER TABLE `pzrs_mapy`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=10;
+ALTER TABLE `pzrs_maps`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
 --
 -- AUTO_INCREMENT pro tabulku `pzrs_matches`
 --
 ALTER TABLE `pzrs_matches`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=60;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=93;
 --
 -- AUTO_INCREMENT pro tabulku `pzrs_posts`
 --
 ALTER TABLE `pzrs_posts`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT pro tabulku `pzrs_settings`
 --
@@ -362,12 +358,12 @@ ALTER TABLE `pzrs_settings`
 -- AUTO_INCREMENT pro tabulku `pzrs_teams`
 --
 ALTER TABLE `pzrs_teams`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=33;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
 --
 -- AUTO_INCREMENT pro tabulku `pzrs_users`
 --
 ALTER TABLE `pzrs_users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
